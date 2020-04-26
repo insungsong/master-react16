cde50da056e6786cfb0ddeda524d36c59fc22e13
insungsong <song22861@naver.com>
2020-04-26 16:46:11 +0900

    Error Boudaryies and Higher Order Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React, { Component, Fragment } from "react";
 import { createPortal } from "react-dom";
 
+const BoundaryHOC = (ProtectedComponent) =>
+  class Boundary extends Component {
+    state = {
+      hasError: false
+    };
+    componentDidCatch = () => {
+      this.setState({
+        hasError: true
+      });
+    };
+    render() {
+      const { hasError } = this.state;
+      if (hasError) {
+        return <ErrorFallback />;
+      } else {
+        return <ProtectedComponent />;
+      }
+    }
+  };
+
+class ErrorMaker extends Component {
+  state = {
+    friends: ["insung", "gildong", "minsu"]
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  }
+
+  render() {
+    const { friends } = this.state;
+    return friends.map((friend) => `${friend} `);
+  }
+}
+
+const PErrorMaker = BoundaryHOC(ErrorMaker);
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById("touchme"));
   }
 }
+
+const PPortals = BoundaryHOC(Portals);
 
 const Message = () => "Just touched this";
 
@@ -15,13 +58,16 @@ class ReturnTypes extends Component {
   }
 }
 
-class App extends React.Component {
+const ErrorFallback = () => "Sorry something went wrong..";
+
+class App extends Component {
   render() {
     return (
-      <>
+      <Fragment>
         <ReturnTypes />
-        <Portals />
-      </>
+        <PPortals />
+        <PErrorMaker />
+      </Fragment>
     );
   }
 }
